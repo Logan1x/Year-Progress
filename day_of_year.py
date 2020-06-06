524954f28d3c2346a85953c8e0b11b855e1a87b4
noob <sharmakhushal78@gmail.com>
2020-06-06 18:02:08 +0530

    version 2 with navbar and 404 page

--- day_of_year.py
+++ day_of_year.py
@@ -34,6 +34,10 @@ def index():
 def abc():
     return render_template("abc.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
